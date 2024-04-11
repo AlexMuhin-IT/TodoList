62c9b12a5805b83e3be003f7b0660c8813db8020
alexMuhin <myx87@bk.ru>
2024-04-11 15:45:47 +0300

    рефакторинг проекта

--- src/features/todolists/ui/Todolists/Todolist/Todolist.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Todolist.tsx
@@ -10,19 +10,37 @@ import { DomainTodolist } from "features/todolists/lib/types/types"
 export type TodolistPropsType = {
   todolist: DomainTodolist
 }
-export const Todolist = ({ todolist }: TodolistPropsType) => {
-  const [addTask] = useCreateTaskMutation()
+export const Todolist = ({
+  todolist,
+}: TodolistPropsType) => {
+  const [addTask] =
+    useCreateTaskMutation()
 
-  const addTaskCallback = (title: string) => {
-    addTask({ todolistId: todolist.id, title })
+  const addTaskCallback = (
+    title: string,
+  ) => {
+    addTask({
+      todolistId: todolist.id,
+      title,
+    })
   }
 
   return (
     <div className={s.wrapper}>
-      <TodolistTitle todolist={todolist} />
-      <AddItemForm addItem={addTaskCallback} disabled={todolist.entityStatus === "loading"} />
+      <TodolistTitle
+        todolist={todolist}
+      />
+      <AddItemForm
+        addItem={addTaskCallback}
+        disabled={
+          todolist.entityStatus ===
+          "loading"
+        }
+      />
       <Tasks todolist={todolist} />
-      <FilterTasksButtons todolist={todolist} />
+      <FilterTasksButtons
+        todolist={todolist}
+      />
     </div>
   )
 }
