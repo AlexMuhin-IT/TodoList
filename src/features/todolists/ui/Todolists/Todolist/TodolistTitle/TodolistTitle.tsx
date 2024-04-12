d18c7c9e62fee8ef64186afa85e086c8951e7367
alexMuhin <myx87@bk.ru>
2024-04-12 15:46:01 +0300

    рефакторинг проекта

--- src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -4,7 +4,11 @@ import { Delete } from "@mui/icons-material"
 import s from "./TodolistTitle.module.css"
 import { useAppDispatch } from "common/hooks"
 import { EditableSpan } from "common/components"
-import { todolistsApi, useDeleteTodolistMutation, useUpdateTodolistTitleMutation } from "features/todolists/api/todolistsApi"
+import {
+  todolistsApi,
+  useDeleteTodolistMutation,
+  useUpdateTodolistTitleMutation,
+} from "features/todolists/api/todolistsApi"
 import { RequestStatus } from "app/appSlice"
 import { DomainTodolist } from "features/todolists/lib/types/types"
 
@@ -12,20 +16,33 @@ type Props = {
   todolist: DomainTodolist
 }
 
-export const TodolistTitle = ({ todolist }: Props) => {
-  const [removeTodolist] = useDeleteTodolistMutation()
-  const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
+export const TodolistTitle = ({
+  todolist,
+}: Props) => {
+  const [removeTodolist] =
+    useDeleteTodolistMutation()
+  const [updateTodolistTitle] =
+    useUpdateTodolistTitleMutation()
   const { title, id } = todolist
   const dispatch = useAppDispatch()
 
-  const updateQueryData = (status: RequestStatus) => {
+  const updateQueryData = (
+    status: RequestStatus,
+  ) => {
     dispatch(
-      todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
-        const index = state.findIndex((tl) => tl.id === id)
-        if (index !== -1) {
-          state[index].entityStatus = status
-        }
-      }),
+      todolistsApi.util.updateQueryData(
+        "getTodolists",
+        undefined,
+        (state) => {
+          const index = state.findIndex(
+            (tl) => tl.id === id,
+          )
+          if (index !== -1) {
+            state[index].entityStatus =
+              status
+          }
+        },
+      ),
     )
   }
   const removeTodolistHandler = () => {
@@ -40,15 +57,37 @@ export const TodolistTitle = ({ todolist }: Props) => {
   //   removeTodolist(id)
   // }
   //
-  const updateTodolistCallback = (title: string) => {
-    updateTodolistTitle({ id: id, title })
+  const updateTodolistCallback = (
+    title: string,
+  ) => {
+    updateTodolistTitle({
+      id: id,
+      title,
+    })
   }
   return (
     <div className={s.container}>
       <h3>
-        <EditableSpan value={title} onChange={updateTodolistCallback} disabled={todolist.entityStatus === "loading"} />
+        <EditableSpan
+          value={title}
+          onChange={
+            updateTodolistCallback
+          }
+          disabled={
+            todolist.entityStatus ===
+            "loading"
+          }
+        />
       </h3>
-      <IconButton aria-label="delete" onClick={removeTodolistHandler} size={"medium"} disabled={todolist.entityStatus === "loading"}>
+      <IconButton
+        aria-label="delete"
+        onClick={removeTodolistHandler}
+        size={"medium"}
+        disabled={
+          todolist.entityStatus ===
+          "loading"
+        }
+      >
         <Delete />
       </IconButton>
     </div>
