a874c55c3f06ce6686abf631eb76d9a7a304e7b1
alexMuhin <myx87@bk.ru>
2024-04-13 15:46:07 +0300

    рефакторинг проекта

--- src/features/todolists/lib/types/types.ts
+++ src/features/todolists/lib/types/types.ts
@@ -1,9 +1,13 @@
 import { Todolist } from "features/todolists/api/todolistsApi.types"
 import { RequestStatus } from "app/appSlice"
 
-export type FilterValues = "all" | "active" | "completed"
+export type FilterValues =
+  | "all"
+  | "active"
+  | "completed"
 
-export type DomainTodolist = Todolist & {
-  filter: FilterValues
-  entityStatus: RequestStatus
-}
+export type DomainTodolist =
+  Todolist & {
+    filter: FilterValues
+    entityStatus: RequestStatus
+  }
