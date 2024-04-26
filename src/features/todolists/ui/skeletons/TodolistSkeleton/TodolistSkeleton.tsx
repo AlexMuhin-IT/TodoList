fcb03d3654ffe7e51a0f458dcc259e3051ba446f
alexMuhin <myx87@bk.ru>
2024-04-26 15:48:31 +0300

    рефакторинг проекта

--- src/features/todolists/ui/skeletons/TodolistSkeleton/TodolistSkeleton.tsx
+++ src/features/todolists/ui/skeletons/TodolistSkeleton/TodolistSkeleton.tsx
@@ -6,49 +6,25 @@ export const TodolistSkeleton = () => {
   return (
     <Paper className={s.todolist}>
       <div className={s.todolistTitle}>
-        <Skeleton
-          width={150}
-          height={50}
-        />
-        <Skeleton
-          width={20}
-          height={40}
-        />
+        <Skeleton width={150} height={50} />
+        <Skeleton width={20} height={40} />
       </div>
 
       <div className={s.addItemForm}>
-        <Skeleton
-          width={230}
-          height={60}
-        />
-        <Skeleton
-          width={20}
-          height={40}
-        />
+        <Skeleton width={230} height={60} />
+        <Skeleton width={20} height={40} />
       </div>
 
       <>
         {Array(4)
           .fill(null)
           .map((_, id) => (
-            <div
-              key={id}
-              className={s.common}
-            >
+            <div key={id} className={s.common}>
               <div className={s.tasks}>
-                <Skeleton
-                  width={20}
-                  height={40}
-                />
-                <Skeleton
-                  width={150}
-                  height={40}
-                />
+                <Skeleton width={20} height={40} />
+                <Skeleton width={150} height={40} />
               </div>
-              <Skeleton
-                width={20}
-                height={40}
-              />
+              <Skeleton width={20} height={40} />
             </div>
           ))}
       </>
@@ -57,11 +33,7 @@ export const TodolistSkeleton = () => {
         {Array(3)
           .fill(null)
           .map((_, id) => (
-            <Skeleton
-              key={id}
-              width={80}
-              height={60}
-            />
+            <Skeleton key={id} width={80} height={60} />
           ))}
       </div>
     </Paper>
