b5f458d9ccc9996266f0091214b49626df2b3da4
alexMuhin <myx87@bk.ru>
2024-04-29 15:48:53 +0300

    рефакторинг проекта

--- src/features/todolists/lib/hooks/useTasks.ts
+++ src/features/todolists/lib/hooks/useTasks.ts
@@ -3,32 +3,22 @@ import { useState } from "react"
 import { useGetTasksQuery } from "features/todolists/api/tasksApi"
 import { TaskStatus } from "common/enums"
 
-export const useTasks = (
-  todolist: DomainTodolist,
-) => {
+export const useTasks = (todolist: DomainTodolist) => {
   const { filter, id } = todolist
 
   const [page, setPage] = useState(1)
 
-  const { data, isLoading } =
-    useGetTasksQuery({
-      todolistId: id,
-      args: { page },
-    })
+  const { data, isLoading } = useGetTasksQuery({
+    todolistId: id,
+    args: { page },
+  })
 
   let tasks = data?.items
   if (filter === "active") {
-    tasks = tasks?.filter(
-      (task) =>
-        task.status === TaskStatus.New,
-    )
+    tasks = tasks?.filter((task) => task.status === TaskStatus.New)
   }
   if (filter === "completed") {
-    tasks = tasks?.filter(
-      (task) =>
-        task.status ===
-        TaskStatus.Completed,
-    )
+    tasks = tasks?.filter((task) => task.status === TaskStatus.Completed)
   }
   return {
     tasks,
