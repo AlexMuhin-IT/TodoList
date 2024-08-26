02709a0e6cca09075e86b11df176599b1431f4e5
alexMuhin <myx87@bk.ru>
2024-08-26 22:26:28 +0300

    начал делать todolist занова

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,18 @@
-import React, {useState} from 'react';
 import './App.css';
-import {TaskType, Todolist} from "./Todolist";
-import {v1, v4} from "uuid";
-
-
-export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
-
-    const [tasks, setTasks] = useState<TaskType[]>([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: true},
-        {id: v1(), title: 'Redux', isDone: false},
-        {id: v1(), title: 'Typescript', isDone: false},
-        {id: v1(), title: 'RTK query', isDone: false},
-    ])
-    const removeTask = (taskId: string) => {
-        const filteredTasks = tasks.filter(t => t.id !== taskId)
-        setTasks(filteredTasks);
-    }
-    const addTask = (title: string) => {
-        const newTask = {
-            id: v4(),
-            title,
-            isDone: false,
-        }
-        const newTasks = [newTask, ...tasks]
-        setTasks(newTasks)
-    }
-    const [filter, setFilter] = useState<FilterValuesType>('all')
-
-    let filteredTasks: Array<TaskType> = tasks
-    if (filter === 'active') {
-        filteredTasks = tasks.filter(t => !t.isDone)
-    }
-    if (filter === 'completed') {
-        filteredTasks = tasks.filter(t => t.isDone)
-    }
-
-    const changeFilter = (filter: FilterValuesType) => {
-        setFilter(filter)
-    }
-    // const tasks2: Array<TaskType> = []
-
+    //
+    // [
+    //     {id: 1, title: 'HTML&CSS', isDone: true},
+    //     {id:2, title: 'JS', isDone: true},
+    //     {id: 3, title: 'ReactJS', isDone: true},
+    //     {id: 4, title: 'Redux', isDone: false},
+    //     {id: 5, title: 'Typescript', isDone: false},
+    //     {id: 6, title: 'RTK query', isDone: false},
+    // ]
     return (
         <div className="App">
-            <Todolist title={'What to learn'}
-                      task={filteredTasks}
-                      removeTask={removeTask}
-                      changeFilter={changeFilter}
-                      addTask={addTask}
-            />
-            {/*<Todolist title={'Songs'} tasks={tasks2}/>*/}
+           <h1>Hello</h1>
         </div>
     );
 }
