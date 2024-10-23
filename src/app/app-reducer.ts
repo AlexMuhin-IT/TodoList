ae9e10046c1139173f49599433f7cd8553ad81d8
alexMuhin <myx87@bk.ru>
2024-10-23 14:22:24 +0300

    start lesson 3 week

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,35 @@
+// export type ThemeMode = 'dark' | 'light'
+//
+// type InitialState = typeof initialState
+//
+// const initialState = {
+//     themeMode: 'light' as ThemeMode,
+// }
+//
+// export const appReducer = (
+//     state: InitialState = initialState,
+//     action: ActionsType
+// ): InitialState => {
+//     switch (action.type) {
+//         case 'CHANGE_THEME': {
+//             return state
+//         }
+//         default:
+//             return state
+//     }
+// }
+//
+// // Action creators
+// // 1
+// export const changeThemeAC = () => {
+//     return {
+//         type: 'CHANGE_THEME',
+//         payload: any
+//     }
+// }
+//
+// // 2
+// // Actions types
+// type ChangeThemeActionType = ReturnType<typeof changeThemeAC>
+//
+// type ActionsType = ChangeThemeActionType
