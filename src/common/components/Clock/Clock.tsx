4da28454543bb18d848beeb6325b704bb72f1b3c
alexMuhin <myx87@bk.ru>
2024-10-29 12:09:53 +0300

    создание часов в проекте

--- src/common/components/Clock/Clock.tsx
+++ src/common/components/Clock/Clock.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import s from './Clock.module.css'
 
+const getTwoDigitalsString = (num: number) => num < 10 ? '0' + num : num;
 export const Clock = () => {
 
     const [date, setDate] = useState(new Date());
+    useEffect(() => {
+        setInterval(() => {
+            setDate(date);
+        }, 1000)
+    }, [])
+
     return (
-        <div>
-            <h4></h4>
-            :
-            <h4></h4>
-            :
-            <h4></h4>
+        <div className={s.container}>
+            <h6>{getTwoDigitalsString(date.getHours())}:</h6>
+            <h6>{getTwoDigitalsString(date.getMinutes())}:</h6>
+            <h6>{getTwoDigitalsString(date.getSeconds())}</h6>
         </div>
     );
 };
