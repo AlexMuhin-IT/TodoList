158c7d360615491eeaa0e3e4cacdb37bd2bbcdc1
alexMuhin <myx87@bk.ru>
2024-11-13 18:17:44 +0300

    refactor Todolist project

---
+++ src/features/todolists/api/tasksApi.types.ts
@@ -0,0 +1,13 @@
+export type Task ={
+  description: string
+  title: string
+  completed: boolean
+  status: number
+  priority: number
+  startDate: string
+  deadline: string
+  id: string
+  todoListId: string
+  order: number
+  addedDate: string
+}
