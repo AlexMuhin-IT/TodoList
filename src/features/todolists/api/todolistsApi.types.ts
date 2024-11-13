a8c1841f0ba52b8679460edafbe046dba219e88a
alexMuhin <myx87@bk.ru>
2024-11-13 18:17:37 +0300

    refactor Todolist project

---
+++ src/features/todolists/api/todolistsApi.types.ts
@@ -0,0 +1,32 @@
+import { FieldError } from "../../../app/AppHttpRequests"
+
+export type Todolist = {
+  id: string
+  title: string
+  addedDate: string
+  order: number
+}
+
+
+export type CreateTodolistResponse = {
+  resultCode: number
+  messages: string[]
+  fieldsErrors: FieldError[]
+  data: {
+    item: Todolist
+  }
+}
+
+export type DeleteTodolistResponse = {
+  resultCode: number
+  messages: string[]
+  fieldsErrors: FieldError[]
+  data: {}
+}
+
+export type UpdateTodolistResponse = {
+  resultCode: number
+  messages: string[]
+  fieldsErrors: FieldError[]
+  data: {}
+}
