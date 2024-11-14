5ec9f63532baeb423ae4c731468422edbad903a2
alexMuhin <myx87@bk.ru>
2024-11-14 15:43:35 +0300

    refactor Todolist project

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -1,7 +1,14 @@
-export type Task ={
+import { FieldError } from "../../../app/AppHttpRequests"
+
+export type GetTasksResponse = {
+  error: string | null
+  totalCount: number
+  items: DomainTask[]
+}
+export type DomainTask ={
   description: string
   title: string
-  completed: boolean
+  // completed: boolean
   status: number
   priority: number
   startDate: string
@@ -10,4 +17,27 @@ export type Task ={
   todoListId: string
   order: number
   addedDate: string
+}
+export type CreateTaskResponse = {
+  resultCode: number
+  messages: string[]
+  fieldsErrors: FieldError[]
+  data: {
+    item: DomainTask
+  }
+}
+export type UpdateTaskResponse={
+  status: number
+  title: string
+  deadline: string
+  description: string
+  priority: number,
+  startDate: string
+}
+export type DeleteTaskResponse={
+  resultCode: number,
+  message: string[],
+  data: {
+    item: DomainTask
+  },
 }
