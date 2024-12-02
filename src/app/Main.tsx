fdc088596048084ca1a0187ebca3c64729db3ae9
alexMuhin <myx87@bk.ru>
2024-12-03 00:18:00 +0300

    выполнение 16 lesson

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Grid from "@mui/material/Grid2"
 import { AddItemForm } from "common/components"
 import { addTodolistTC } from "../features/todolists/model/todolist-reducer"
 import { Container } from "@mui/material"
-import { useAppDispatch } from "common/hooks"
+import { useAppDispatch, useAppSelector } from "common/hooks"
 import Todolists from "../features/todolists/ui/Todolists/Todolists"
+import { Path } from "common/routing/Routing"
+import { selectIsLoggedIn } from "app/authSelector"
+import { useNavigate } from "react-router"
 
 export const Main = () => {
   const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+  const navigate = useNavigate()
+
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(Path.Login)
+    }
+  }, [isLoggedIn])
 
   const addTodolist = ( title: string ) => {
     dispatch(addTodolistTC(title))
