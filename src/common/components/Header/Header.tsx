7f266af6b48cf492115b2b81ff36375afe7644ce
alexMuhin <myx87@bk.ru>
2024-12-03 00:17:41 +0300

    выполнение 16 lesson

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import AppBar from "@mui/material/AppBar"
 import { IconButton, LinearProgress, Switch, Toolbar } from "@mui/material"
-import { Menu } from "@mui/icons-material"
+import { Logout, Menu } from "@mui/icons-material"
 import { MenuButton } from "../MenuButton/MenuButton"
 import React from "react"
 import { getTheme } from "../../theme/theme"
@@ -8,16 +8,21 @@ import { changeThemeAC } from "app/app-reducer"
 import { useAppDispatch, useAppSelector } from "common/hooks"
 import { selectStatus, selectThemeMode } from "app/appSelectors"
 import { Clock } from "../Clock/Clock"
+import { selectIsLoggedIn } from "app/authSelector"
+import { logoutTC } from "../../../features/auth/model/auth-reducer"
 
 export const Header = () => {
   const themeMode = useAppSelector(selectThemeMode)
   const status = useAppSelector(selectStatus)
-
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const theme = getTheme(themeMode)
   const dispatch = useAppDispatch()
 
   const changeModeHandler = () => {
     dispatch(changeThemeAC(themeMode == "light" ? "dark" : "light"))
+  }
+  const logoutHandler = () => {
+    dispatch(logoutTC())
   }
   return (
     <AppBar sx={{ mb: "30px" }} position="static">
@@ -27,14 +32,15 @@ export const Header = () => {
           <Clock />
         </IconButton>
         <div>
-          <MenuButton>Login</MenuButton>
-          <MenuButton>Logout</MenuButton>
+          {/*<MenuButton>Login</MenuButton>*/}
+          {isLoggedIn && <MenuButton onClick={logoutHandler}>Logout</MenuButton>}
+          {/*<MenuButton onClick={logoutHandler}>Logout</MenuButton>*/}
           <MenuButton background={theme.palette.primary.dark}>Faq</MenuButton>
           <Switch color={"default"} onChange={changeModeHandler} />
         </div>
       </Toolbar>
       {
-        status === 'loading' && <LinearProgress color="error" />
+        status === "loading" && <LinearProgress color="error" />
       }
     </AppBar>
   )
