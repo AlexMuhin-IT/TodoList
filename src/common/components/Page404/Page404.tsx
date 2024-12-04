d94e7b029c4484a78ba6df5fceaf95c4b9929092
alexMuhin <myx87@bk.ru>
2024-12-04 21:17:00 +0300

    выполнение 16 lesson

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,10 +1,20 @@
-import s from './Page404.module.css'
+import s from "./Page404.module.css"
+import { Button, Link } from "@mui/material"
+import { Path } from "common/routing/Routing"
+import { Route, Routes } from "react-router"
 
 export const Page404 = () => {
   return (
-    <>
-      <h1 className={s.title}>404</h1>
-      <h2 className={s.subTitle}>page not found</h2>
-    </>
+    <div  className={s.container}>
+      {/*<h1 className={s.title}>404</h1>*/}
+      <img  alt={''} className={s.img} />
+      {/*<h2 className={s.subTitle}>page not found</h2>*/}
+      <h2 className={s.subTitle}>нет такой страницы</h2>
+      <Routes>
+        <Route path="/*" element={
+          <Button className={s.button} href={Path.Main} variant="contained" size={'large'} sx={{ m: 'auto'}}
+          >Todolist's</Button>} />
+      </Routes>
+    </div>
   )
 }
