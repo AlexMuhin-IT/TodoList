308176ce472b3ff268e3b96bf65302b5c54e0504
alexMuhin <myx87@bk.ru>
2024-12-04 21:17:03 +0300

    выполнение 16 lesson

---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router"
+import { Login } from "../../features/auth/ui/Login/Login"
+import { Main } from "app/Main"
+import { Page404 } from "common/components/Page404/Page404"
+
+export const Path = {
+  Main: '/',
+  Login: 'login',
+  Error: '*',
+} as const
+
+
+export const Routing = () => {
+  return (
+    <Routes>
+      <Route path={Path.Main} element={<Main />} />
+      <Route path={Path.Login} element={<Login />} />
+      <Route path={Path.Error} element={<Page404 />} />
+    </Routes>
+  )
+}
