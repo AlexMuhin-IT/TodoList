fd7d4b2e7cd7cd9216205e9b18dd5ccee03704a2
alexMuhin <myx87@bk.ru>
2024-12-07 23:00:05 +0300

    выполнение 17 lesson

--- src/app/appSelectors.ts
+++
@@ -1,8 +0,0 @@
-import { RootState } from "./store"
-
-export const selectTodolists = (state: RootState) => state.todolist
-export const selectTasks = (state: RootState) => state.tasks
-
-export const selectThemeMode = (state: RootState) => state.app.themeMode
-export const selectStatus = (state: RootState) => state.app.status
-export const selectError = (state: RootState) => state.app.error
