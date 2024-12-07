995d6cf800f7e804a76080a54f44658590135aa2
alexMuhin <myx87@bk.ru>
2024-12-07 23:00:01 +0300

    выполнение 17 lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,11 @@ import { CircularProgress, CssBaseline, ThemeProvider } from "@mui/material"
 import { getTheme } from "common/theme/theme"
 import { Header } from "common/components/Header/Header"
 import { useAppDispatch, useAppSelector } from "common/hooks"
-import { selectThemeMode } from "./appSelectors"
 import { ErrorSnackbar } from "common/components"
 import { Routing } from "common/routing/Routing"
-import { initializeAppTC } from "features/auth/model/auth-reducer"
-import { selectIsInitialized } from "app/authSelector"
+import { initializeAppTC, selectIsInitialized } from "features/auth/model/authSlice"
 import s from "./App.module.css"
+import { selectThemeMode } from "app/appSlice"
 
 export const App = () => {
   const dispatch = useAppDispatch()
