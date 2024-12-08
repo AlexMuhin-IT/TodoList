00b589b85bc332eb7c979e9fcf172f102cfb0323
alexMuhin <myx87@bk.ru>
2024-12-08 20:40:09 +0300

    выполнение 17 lesson

--- src/common/utils/handleServerAppError.ts
+++ src/common/utils/handleServerAppError.ts
@@ -1,12 +1,12 @@
-import { Dispatch } from 'redux'
-import { setAppErrorAC, setAppStatusAC } from "app/app-reducer"
+import { Dispatch } from "redux"
 import { BaseResponse } from "common/types/types"
+import { setAppError, setAppStatus } from "app/appSlice"
 
-export const handleServerAppError = <T,>(data: BaseResponse<T>, dispatch: Dispatch) => {
+export const handleServerAppError = <T>(data: BaseResponse<T>, dispatch: Dispatch) => {
   if (data.messages.length) {
-    dispatch(setAppErrorAC(data.messages[0]))
+    dispatch(setAppError({ error: data.messages[0] }))
   } else {
-    dispatch(setAppErrorAC('Some error occurred'))
+    dispatch(setAppError({ error: "Some error occurred" }))
   }
-  dispatch(setAppStatusAC('failed'))
+  dispatch(setAppStatus({ status: "failed" }))
 }
