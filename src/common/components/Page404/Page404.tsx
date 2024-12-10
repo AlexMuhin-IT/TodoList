5475e541f953090fab7edb85b534c2fdae825ffe
alexMuhin <myx87@bk.ru>
2024-12-10 18:15:01 +0300

    выполнение 17 lesson

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,20 +1,26 @@
 import s from "./Page404.module.css"
-import { Button, Link } from "@mui/material"
+import { Button } from "@mui/material"
 import { Path } from "common/routing/Routing"
 import { Route, Routes } from "react-router"
 
 export const Page404 = () => {
   return (
-    <div  className={s.container}>
+    <div className={s.container}>
       {/*<h1 className={s.title}>404</h1>*/}
-      <img  alt={''} className={s.img} />
+      <img alt={""} className={s.img} />
       {/*<h2 className={s.subTitle}>page not found</h2>*/}
       <h2 className={s.subTitle}>нет такой страницы</h2>
-      <Routes>
-        <Route path="/*" element={
-          <Button className={s.button} href={Path.Main} variant="contained" size={'large'} sx={{ m: 'auto'}}
-          >Todolist's</Button>} />
-      </Routes>
+      <Button className={s.button} href={Path.Main} variant="contained" size={"large"} sx={{ m: "auto" }}>
+        Todolist's
+      </Button>
+
+      {/*<Routes>*/}
+      {/*  <Route*/}
+      {/*    path="/*"*/}
+      {/*    // element={*/}
+      {/*    // }*/}
+      {/*  />*/}
+      {/*</Routes>*/}
     </div>
   )
 }
