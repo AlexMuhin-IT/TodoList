43208e8c93c6f9699cf9d7bc49c50092c3296c65
alexMuhin <myx87@bk.ru>
2024-12-13 00:09:26 +0300

    выполнение 17 lesson

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -11,13 +11,12 @@ export const todolistsApi = {
     return instance.post<BaseResponse<{ item: Todolist }>>("todo-lists", { title })
   },
 
-  updateTodolist(payload: { id: string, title: string }) {
+  updateTodolist(payload: { id: string; title: string }) {
     const { title, id } = payload
     return instance.put<BaseResponse>(`todo-lists/${id}`, { title })
   },
 
   deleteTodolist(id: string) {
     return instance.delete<BaseResponse>(`todo-lists/${id}`)
-  }
+  },
 }
-
