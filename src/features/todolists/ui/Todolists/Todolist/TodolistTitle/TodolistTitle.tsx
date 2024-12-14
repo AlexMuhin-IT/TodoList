5d25ba0a5d002275cd5afa0f2380e52003de0c9b
alexMuhin <myx87@bk.ru>
2024-12-14 17:06:26 +0300

    выполнение 17 lesson

--- src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -3,7 +3,7 @@ import { IconButton } from "@mui/material"
 import { Delete } from "@mui/icons-material"
 import s from "./TodolistTitle.module.css"
 import { useAppDispatch } from "common/hooks"
-import { DomainTodolist, removeTodolistTC, updateTodolistTitleTC } from "../../../../model/todolist-reducer"
+import { DomainTodolist, removeTodolistTC, updateTodolistTitleTC } from "features/todolists/model/todolistsSlice"
 import { EditableSpan } from "common/components"
 
 type Props = {
@@ -25,14 +25,14 @@ export const TodolistTitle = ({ todolist }: Props) => {
   return (
     <div className={s.container}>
       <h3>
-        <EditableSpan value={title}
-                      onChange={updateTodolist}
-                      disabled={todolist.entityStatus === "loading"}/>
+        <EditableSpan value={title} onChange={updateTodolist} disabled={todolist.entityStatus === "loading"} />
       </h3>
-      <IconButton aria-label="delete"
-                  onClick={removeTodolist}
-                  size={"medium"}
-                  disabled={todolist.entityStatus === 'loading'}>
+      <IconButton
+        aria-label="delete"
+        onClick={removeTodolist}
+        size={"medium"}
+        disabled={todolist.entityStatus === "loading"}
+      >
         <Delete />
       </IconButton>
     </div>
