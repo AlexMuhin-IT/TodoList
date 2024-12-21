86ef7541b2eeaeaf4d882195cb047f08b671b7c3
alexMuhin <myx87@bk.ru>
2024-12-22 00:51:01 +0300

    выполнение 18 lesson

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,36 +1,24 @@
 import { UnknownAction } from "redux"
-import {
-  tasksSlice,
-  todolistsReducer,
-} from "features/todolists/model"
+import { tasksSlice, todolistsReducer } from "features/todolists/model"
 import { appReducer, appSlice } from "app/appSlice"
 import { ThunkAction } from "redux-thunk"
-import {
-  authReducer,
-  authSlice,
-} from "features/auth/model/authSlice"
 import { configureStore } from "@reduxjs/toolkit"
 import { tasksReducer } from "features/todolists/model/taskSlice"
 import { todolistsSlice } from "features/todolists/model/todolistsSlice"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "app/baseApi"
 
 export const store = configureStore({
   reducer: {
     [todolistsSlice.name]: todolistsReducer,
     [tasksSlice.name]: tasksReducer,
     [appSlice.name]: appReducer,
-    [authSlice.name]: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
+setupListeners(store.dispatch)
+
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
-export type AppThunk = ThunkAction<
-  void,
-  RootState,
-  unknown,
-  UnknownAction
->
-
-// @ts-ignore
-window.store = store
