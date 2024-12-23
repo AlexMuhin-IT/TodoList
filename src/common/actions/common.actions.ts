3a32d8cf8d282003aad5ad06b69e3012522bba79
alexMuhin <myx87@bk.ru>
2024-12-23 13:11:43 +0300

    выполнение 18 lesson

--- src/common/actions/common.actions.ts
+++ src/common/actions/common.actions.ts
@@ -1,6 +1,4 @@
 import { createAction } from "@reduxjs/toolkit"
-import { TasksStateType } from "features/todolists/model/taskSlice"
-import { DomainTodolist } from "features/todolists/model/todolistsSlice"
 
 // export type ClearTasksAndTodolists = {
 //   tasks: TasksStateType
