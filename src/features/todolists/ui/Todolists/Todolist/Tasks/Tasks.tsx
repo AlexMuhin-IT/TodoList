feff3b0b867d86d02575e80eba937f68540e2e2a
alexMuhin <myx87@bk.ru>
2024-12-24 08:29:12 +0300

    выполнение 18 lesson

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,34 +1,36 @@
 import React, { useEffect } from "react"
 import { List } from "@mui/material"
-import { useAppDispatch, useAppSelector } from "common/hooks"
+import { useAppDispatch } from "common/hooks"
 import { TaskStatus } from "common/enums"
 import { DomainTodolist } from "features/todolists/model/todolistsSlice"
-import { fetchTasksTC, selectTasks } from "features/todolists/model/taskSlice"
 import { Task } from "features/todolists/ui/Todolists/Todolist/Tasks/Task/Task"
+import { useGetTasksQuery } from "features/todolists/api/tasksApi"
 
 type Props = {
   todolist: DomainTodolist
 }
 
 export const Tasks = ({ todolist }: Props) => {
-  const tasks = useAppSelector(selectTasks)
-
+  // const tasks = useAppSelector(selectTasks)
+  const { data } = useGetTasksQuery({ todolistId: todolist.id })
   const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    dispatch(fetchTasksTC(todolist.id))
-  }, [])
+  // useEffect(() => {
+  //   dispatch(fetchTasksTC(todolist.id))
+  // }, [])
 
-  const allTodolistTasks = tasks[todolist.id]
+  let taskForTodolist = data?.items
 
-  let taskForTodolist = allTodolistTasks
+  // const allTodolistTasks = tasks[todolist.id]
+
+  // let taskForTodolist = allTodolistTasks
 
   if (todolist.filter === "active") {
-    taskForTodolist = allTodolistTasks.filter((task) => task.status === TaskStatus.New)
+    taskForTodolist = taskForTodolist?.filter((task) => task.status === TaskStatus.New)
   }
 
   if (todolist.filter === "completed") {
-    taskForTodolist = allTodolistTasks.filter((task) => task.status === TaskStatus.Completed)
+    taskForTodolist = taskForTodolist?.filter((task) => task.status === TaskStatus.Completed)
   }
   return (
     <>
