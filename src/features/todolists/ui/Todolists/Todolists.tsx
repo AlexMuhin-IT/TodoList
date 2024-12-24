8876a58761654ea661863c9c1e7b53bce002a7a5
alexMuhin <myx87@bk.ru>
2024-12-25 00:09:08 +0300

    выполнение 19 lesson

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -3,9 +3,22 @@ import Grid from "@mui/material/Grid2"
 import { Paper } from "@mui/material"
 import { Todolist } from "features/todolists/ui/Todolists/Todolist/Todolist"
 import { useGetTodolistsQuery } from "features/todolists/api/todolistsApi"
+import { TodolistSkeleton } from "features/todolists/ui/skeletons/TodolistSkeleton/TodolistSkeleton"
 
 const Todolists = () => {
-  const { data: todolists } = useGetTodolistsQuery()
+  const { data: todolists, isLoading } = useGetTodolistsQuery()
+
+  if (isLoading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "32px" }}>
+        {Array(3)
+          .fill(null)
+          .map((_, id) => (
+            <TodolistSkeleton key={id} />
+          ))}
+      </div>
+    )
+  }
 
   return (
     <>
