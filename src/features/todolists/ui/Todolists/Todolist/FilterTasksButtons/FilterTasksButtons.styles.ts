cb7cc797356c0d296627aef97acdd01f599b4810
alexMuhin <myx87@bk.ru>
2024-12-26 14:27:51 +0300

    выполнение 19 lesson

--- src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.styles.ts
+++ src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.styles.ts
@@ -1,6 +1,14 @@
-import { SxProps } from "@mui/material"
-
-export const filterButtonsContainerSx: SxProps = () => ({
-  display: "flex",
-  justifyContent: "space-around",
-})
+// import { SxProps } from "@mui/material"
+//
+// export const filterButtonsContainerSx: SxProps = () => ({
+//   display: "flex",
+//   justifyContent: "space-around",
+//   color: "secondary",
+//   "&:active": {
+//     boxShadow: "none",
+//     backgroundColor: "#0062cc",
+//     borderColor: "#005cbf",
+//     borderBlockColor: "black",
+//   },
+//
+// })
--- src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
+++ src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import { filterButtonsContainerSx } from "./FilterTasksButtons.styles"
-import { Box, Button } from "@mui/material"
+import React, { useState } from "react"
 import { useAppDispatch } from "common/hooks"
-import { DomainTodolist, FilterValuesType } from "features/todolists/model/todolistsSlice"
 import { todolistsApi } from "features/todolists/api/todolistsApi"
+import { DomainTodolist, FilterValues } from "features/todolists/lib/types/types"
+
+import { styled } from "@mui/material/styles"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
 
 type Props = {
   todolist: DomainTodolist
@@ -12,8 +14,9 @@ type Props = {
 export const FilterTasksButtons = ({ todolist }: Props) => {
   const dispatch = useAppDispatch()
   const { id } = todolist
+  const [selectedFilter, setSelectedFilter] = useState<FilterValues>("all")
 
-  const changeFilterTasksHandler = (filter: FilterValuesType) => {
+  const changeFilterTasksHandler = (filter: FilterValues) => {
     dispatch(
       todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
         const index = state.findIndex((tl) => tl.id === id)
@@ -22,38 +25,178 @@ export const FilterTasksButtons = ({ todolist }: Props) => {
         }
       }),
     )
+    setSelectedFilter(filter) // Обновляем выбранный фильтр
   }
 
+  const BootstrapButton = styled(Button)(({ isActive }: { isActive: boolean }) => ({
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 12,
+    padding: "6px 12px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: isActive ? "#00d91d" : "#00d91d",
+    borderColor: isActive ? "#00d91d" : "green",
+    color: isActive ? "white" : "inherit",
+    "&:hover": {
+      backgroundColor: isActive ? "#00d91d" : "#0063cc",
+      borderColor: "#00d91d",
+      boxShadow: "none",
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    },
+  }))
+
   return (
-    <Box sx={filterButtonsContainerSx}>
-      <Button
-        color="secondary"
+    <Stack spacing={3} direction="row">
+      <BootstrapButton
         size={"small"}
         variant="contained"
-        className={todolist.filter === "all" ? "active-filter" : ""}
+        disableRipple
+        isActive={selectedFilter === "all"}
         onClick={() => changeFilterTasksHandler("all")}
       >
-        {" "}
         All
-      </Button>
-      <Button
-        color="secondary"
+      </BootstrapButton>
+      <BootstrapButton
         size={"small"}
         variant="contained"
-        className={todolist.filter === "active" ? "active-filter" : ""}
+        disableRipple
+        isActive={selectedFilter === "active"}
         onClick={() => changeFilterTasksHandler("active")}
       >
         Active
-      </Button>
-      <Button
-        color="secondary"
+      </BootstrapButton>
+      <BootstrapButton
         size={"small"}
         variant="contained"
-        className={todolist.filter === "completed" ? "active-filter" : ""}
+        disableRipple
+        isActive={selectedFilter === "completed"}
         onClick={() => changeFilterTasksHandler("completed")}
       >
         Completed
-      </Button>
-    </Box>
+      </BootstrapButton>
+    </Stack>
   )
 }
+
+//   const BootstrapButton = styled(Button)({
+//     isActive: true,
+//     ":active": "#00d91d",
+//     boxShadow: "none",
+//     textTransform: "none",
+//     fontSize: 16,
+//     padding: "6px 12px",
+//     border: "1px solid",
+//     lineHeight: 1.5,
+//     // backgroundColor: "#0063cc",
+//     backgroundColor: "secondary",
+//     borderColor: "green",
+//     fontFamily: [
+//       "-apple-system",
+//       "BlinkMacSystemFont",
+//       '"Segoe UI"',
+//       "Roboto",
+//       '"Helvetica Neue"',
+//       "Arial",
+//       "sans-serif",
+//       '"Apple Color Emoji"',
+//       '"Segoe UI Emoji"',
+//       '"Segoe UI Symbol"',
+//     ].join(","),
+//     "&:hover": {
+//       backgroundColor: "#00d91d",
+//       borderColor: "#00d91d",
+//       boxShadow: "none",
+//     },
+//     "&:active": {
+//       boxShadow: "none",
+//       backgroundColor: "#0062cc",
+//       // backgroundColor: "secondary",
+//       borderColor: "#005cbf",
+//     },
+//     "&:focus": {
+//       boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+//     },
+//   })
+//   return (
+// <Box sx={filterButtonsContainerSx}>
+//   <Button
+//     // color="secondary"
+//     size={"small"}
+//     variant="contained"
+//     className={todolist.filter === "all" ? "active-filter" : ""}
+//     onClick={() => changeFilterTasksHandler("all")}
+//   >
+//     All
+//   </Button>
+//   <Button
+//     // color="secondary"
+//     size={"small"}
+//     variant="contained"
+//     className={todolist.filter === "active" ? "active-filter" : ""}
+//     onClick={() => changeFilterTasksHandler("active")}
+//   >
+//     Active
+//   </Button>
+//   <Button
+//     // color="secondary"
+//     size={"small"}
+//     variant="contained"
+//     className={todolist.filter === "completed" ? "active-filter" : ""}
+//     onClick={() => changeFilterTasksHandler("completed")}
+//   >
+//     Completed
+//   </Button>
+// </Box>
+//     <Stack spacing={3} direction="row">
+//       <BootstrapButton
+//         size={"small"}
+//         variant="contained"
+//         disableRipple
+//         className={todolist.filter === "all" ? "active-filter" : ""}
+//         onClick={() => changeFilterTasksHandler("all")}
+//       >
+//         All
+//       </BootstrapButton>
+//       <BootstrapButton
+//         size={"small"}
+//         variant="contained"
+//         disableRipple
+//         className={todolist.filter === "active" ? "active-filter" : ""}
+//         onClick={() => changeFilterTasksHandler("active")}
+//       >
+//         Active
+//       </BootstrapButton>
+//       <BootstrapButton
+//         size={"small"}
+//         variant="contained"
+//         disableRipple
+//         className={todolist.filter === "completed" ? "active-filter" : ""}
+//         onClick={() => changeFilterTasksHandler("completed")}
+//       >
+//         Completed
+//       </BootstrapButton>
+//     </Stack>
+//   )
+// }
+
+// const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+//   color: theme.palette.getContrastText(purple[500]),
+//   backgroundColor: purple[500],
+//   "&:hover": {
+//     backgroundColor: purple[700],
+//   },
+// }))
+
+// export default function CustomizedButtons() {
+//   return (
+//     <Stack spacing={2} direction="row">
+//       <ColorButton variant="contained">Custom CSS</ColorButton>
+//       <BootstrapButton variant="contained" disableRipple>
+//         Bootstrap
+//       </BootstrapButton>
+//     </Stack>
+//   );
+// }
