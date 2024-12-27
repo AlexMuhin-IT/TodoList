a7604b4201acbf8f39e7cd8299c95d163d9cd519
alexMuhin <myx87@bk.ru>
2024-12-27 10:14:11 +0300

    выполнение 19 lesson

--- src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.styles.ts
+++ src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.styles.ts
@@ -12,3 +12,26 @@
 //   },
 //
 // })
+import { styled } from "@mui/material/styles"
+import Button from "@mui/material/Button"
+
+export const BootstrapButton = styled(Button)(({ isActive }: { isActive: boolean }) => ({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 12,
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  // color: "white",
+  backgroundColor: isActive ? "#00d91d" : "#00d91d",
+  borderColor: isActive ? "#00d91d" : "green",
+  color: isActive ? "white" : "inherit",
+  "&:hover": {
+    backgroundColor: isActive ? "#00d91d" : "#0063cc",
+    borderColor: "#00d91d",
+    boxShadow: "none",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+}))
