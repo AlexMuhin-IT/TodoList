0b3da1bae9dff051883145e60c5a3efab6af43aa
alexMuhin <myx87@bk.ru>
2025-01-03 18:46:49 +0300

    выполнение 19 lesson

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,10 @@
-import { tasksSlice, todolistsReducer } from "features/todolists/model"
 import { appReducer, appSlice } from "app/appSlice"
 import { configureStore } from "@reduxjs/toolkit"
-import { tasksReducer } from "features/todolists/model/taskSlice"
-import { todolistsSlice } from "features/todolists/model/todolistsSlice"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import { baseApi } from "app/baseApi"
 
 export const store = configureStore({
   reducer: {
-    [todolistsSlice.name]: todolistsReducer,
-    [tasksSlice.name]: tasksReducer,
     [appSlice.name]: appReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
