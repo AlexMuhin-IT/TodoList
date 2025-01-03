55441e50190e841b46c08a882ea82393e7bff5d0
alexMuhin <myx87@bk.ru>
2025-01-03 18:47:06 +0300

    выполнение 19 lesson

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -17,12 +17,12 @@ export type DomainTask = {
   order: number
   addedDate: string
 }
-
-export type UpdateTaskDomainModel = {
-  title?: string
-  description?: string
-  status?: TaskStatus
-  priority?: TaskPriority
-  startDate?: string
-  deadline?: string
-}
+//
+// export type UpdateTaskDomainModel = {
+//   title?: string
+//   description?: string
+//   status?: TaskStatus
+//   priority?: TaskPriority
+//   startDate?: string
+//   deadline?: string
+// }
