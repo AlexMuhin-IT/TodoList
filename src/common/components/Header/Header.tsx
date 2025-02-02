fce544c60f5e10bd913f057aafd2a4ab007d83de
alexMuhin <myx87@bk.ru>
2025-02-02 18:34:25 +0300

    рефакторинг проекта

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -72,7 +72,6 @@ export const Header = () => {
               position: "absolute",
               top: 115,
               left: 0,
-
               right: 0,
               zIndex: 10,
             }}
